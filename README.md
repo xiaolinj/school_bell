666c77c69bbfcaac052a54a275f788a5e2264945
xlinlin23 <xiaolinjiang6@gmail.com>
2021-02-14 16:24:11 -0800

    draft

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # school_bell
+It is a simulation of school bell ringing. Idealy, it will have class time of 45 mins and a 15 min break. Still WIP.
---
+++ bell.py
@@ -0,0 +1,33 @@
+import time
+from playsound import playsound
+import csv
+import sys
+
+def get_timetable():
+    with open('timetable.csv', 'r') as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader]
+    return rows
+
+
+def bell_and_toast(list_timetable):
+    class_begin = []
+    class_end = []
+    for line in list_timetable[1:]:
+        class_begin.append(line[1]) 
+        class_end.append(line[2])
+    length = len(list_timetable) - 1
+    class_index = 0
+    while class_index < length:
+        now_localtime = time.strftime('%H:%M:%S', time.localtime())
+        if now_localtime in class_begin:
+            class_index = class_begin.index(now_localtime) + 1
+            playsound('bells.mp3')
+        if now_localtime in class_end:
+            playsound('bells.mp3')
+            
+
+if __name__ == "__main__":
+    print('running')
+    bell_and_toast(get_timetable())
+    sys.exit()
